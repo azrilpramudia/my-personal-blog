0f08dc1e3346f966c9a7c35aeb5c4b6fc839e2d8
azrilpramudia <azrilpramudia01@gmail.com>
2025-11-20 15:36:22 +0700

    feat: add responsive to navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import { useState } from "react";
-import { Moon, Sun } from "lucide-react";
+import { Moon, Sun, Menu, X } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [dark, setDark] = useState(false);
+  const [open, setOpen] = useState(false);
 
   const toggleTheme = () => {
     setDark(!dark);
@@ -20,38 +21,107 @@ const Navbar = () => {
             The Daily Dev
           </Link>
 
-          {/* Navigation Links */}
-          <div className="flex items-center gap-6 text-gray-600 dark:text-gray-300">
+          {/* Desktop Navigation Links */}
+          <div className="hidden md:flex items-center gap-6 text-gray-600 dark:text-gray-300">
             <Link
               to="/article"
-              className="hover:text-black dark:hover:text-white"
+              className="
+      relative inline-block 
+      hover:text-black dark:hover:text-white
+      after:content-[''] after:absolute after:left-0 after:-bottom-1
+      after:h-0.5 after:w-0 after:bg-current
+      after:transition-all after:duration-300
+      hover:after:w-full
+    "
             >
               Article
             </Link>
+
             <Link
               to="/about"
-              className="hover:text-black dark:hover:text-white"
+              className="
+      relative inline-block 
+      hover:text-black dark:hover:text-white
+      after:content-[''] after:absolute after:left-0 after:-bottom-1
+      after:h-0.5 after:w-0 after:bg-current
+      after:transition-all after:duration-300
+      hover:after:w-full
+    "
             >
               About
             </Link>
           </div>
         </div>
 
-        {/* Right Side */}
+        {/* Right side */}
         <div className="flex items-center gap-6 text-gray-600 dark:text-gray-300">
-          <a href="#" className="hover:text-black dark:hover:text-white">
-            Linkedin
-          </a>
-          <a href="#" className="hover:text-black dark:hover:text-white">
-            Github
-          </a>
+          {/* Desktop Social */}
+          <div className="hidden md:flex items-center gap-6">
+            <a
+              href="#"
+              className="hover:text-black dark:hover:text-white hover:underline underline-offset-4"
+            >
+              Linkedin
+            </a>
+            <a
+              href="#"
+              className="hover:text-black dark:hover:text-white hover:underline underline-offset-4"
+            >
+              Github
+            </a>
+          </div>
 
           {/* Dark Mode Toggle */}
           <button onClick={toggleTheme} className="p-1 hover:opacity-70">
             {dark ? <Sun size={20} /> : <Moon size={20} />}
           </button>
+
+          {/* Mobile menu button */}
+          <button
+            className="md:hidden p-1 hover:opacity-70"
+            onClick={() => setOpen(!open)}
+          >
+            {open ? <X size={24} /> : <Menu size={24} />}
+          </button>
         </div>
       </div>
+
+      {/* Mobile Dropdown */}
+      {open && (
+        <div className="md:hidden border-t py-4 px-4 bg-white dark:bg-gray-900">
+          <div className="flex flex-col gap-4 text-gray-700 dark:text-gray-300">
+            <Link
+              to="/article"
+              onClick={() => setOpen(false)}
+              className="hover:underline underline-offset-4"
+            >
+              Article
+            </Link>
+            <Link
+              to="/about"
+              onClick={() => setOpen(false)}
+              className="hover:underline underline-offset-4"
+            >
+              About
+            </Link>
+
+            <a
+              href="#"
+              onClick={() => setOpen(false)}
+              className="hover:underline underline-offset-4"
+            >
+              Linkedin
+            </a>
+            <a
+              href="#"
+              onClick={() => setOpen(false)}
+              className="hover:underline underline-offset-4"
+            >
+              Github
+            </a>
+          </div>
+        </div>
+      )}
     </nav>
   );
 };
