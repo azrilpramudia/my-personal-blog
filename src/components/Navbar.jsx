7ec63964facf0dcffd64c1485ea829555a27f387
azrilpramudia <azrilpramudia01@gmail.com>
2025-11-20 11:41:19 +0700

    initial setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+const Navbar = () => {
+  return (
+    <nav className="flex justify-center items-center">
+      <h1 className="text-3xl font-bold">Navbar</h1>
+    </nav>
+  );
+};
+
+export default Navbar;
