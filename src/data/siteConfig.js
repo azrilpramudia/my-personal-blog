9098755e28db3dbbf809a4d250f291077597799e
azrilpramudia <azrilpramudia01@gmail.com>
2025-11-21 17:04:49 +0700

    feat: add siteconfig

---
+++ src/data/siteConfig.js
@@ -0,0 +1,8 @@
+export const siteConfig = {
+  name: "The Daily Dev",
+  author: "ItsMeow",
+  links: {
+    github: "https://github.com/yourname",
+    linkedin: "https://linkedin.com/in/yourname",
+  }
+};
